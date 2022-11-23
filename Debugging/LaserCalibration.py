0dcb3eb3176f83d2f2ce0c53fe6d6e9d827c2e9a
basicallyAlexOh <alexander.s.oh@vanderbilt.edu>
2022-11-23 19:36:30 +0000

    Reorganized and added Test Images

--- Debugging/LaserCalibration.py
+++ Debugging/LaserCalibration.py
@@ -1,21 +1,19 @@
 from servo import *
 from picamera2 import Picamera2, Preview
-import cv2
+#import cv2
 
 
 def laser_calibration():
     pwm=Servo()
-    pwm.setServoPwm('0',90)
+    pwm.setServoPwm('0',85)
     pwm.setServoPwm('1',0)
     picam2 = Picamera2()
     camera_config = picam2.create_preview_configuration()
     picam2.configure(camera_config)
     picam2.start()
     time.sleep(5)
-    picam2.capture_file("imageL.jpg")
-
-
-
+    picam2.capture_file("RedLaser20CM10R.jpg")
+    
 
 def main():
     laser_calibration()
