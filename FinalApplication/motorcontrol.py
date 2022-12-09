994aa3df638d73d758d150f2da270ff74977d9d1
Ryan Taylor <ryan.c.taylor@vanderbilt.edu>
2022-12-09 00:59:03 -0600

    adding comments to code c3

--- FinalApplication/motorcontrol.py
+++ FinalApplication/motorcontrol.py
@@ -4,13 +4,14 @@ import math
 import time
 import logging
 
-
+# MotorControl class uses relPos x,y coorndinates to detemine motor controls to follow laser ptr 
 class MotorControl(threading.Thread):
 
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
         super().__init__(group=group, target=target, name=name, daemon=daemon)
         print("Created Motor Controller")
 
+    # run loop that continously generates motor controls 
     def run(self):
         while runner.Runner.initialPath:
             runner.Runner.lock1.acquire()
@@ -40,11 +41,13 @@ class MotorControl(threading.Thread):
             time.sleep(0.02)
         self.set_motors(0,0)
 
+    # converts relPos x,y coordniates to polar form for analysis 
     def to_polar(self, d, x):
         radius = math.sqrt(d*d + x*x)
         theta = math.atan(x/d)
         return radius, theta
 
+    # determines lSpeed and rSpeed based off polar coordinates 
     def set_motors(self, l, r):
         runner.Runner.motor.setMotorModel(l,l,r,r)
         runner.Runner.lSpeed = l
